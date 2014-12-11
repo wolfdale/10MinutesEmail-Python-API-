445a50a2390241bf2bbfd6d0a34382a46861e46c
Prakhar Gupta <mywolfdale@gmail.com>
2014-12-11 19:26:13 +0530

    Update 10MinEmail.py

--- 10MinEmail.py
+++ 10MinEmail.py
@@ -1,13 +1,34 @@
 from bs4 import BeautifulSoup
 import threading
 import urllib
+
+
 web=urllib.urlopen('http://www.my10minutemail.com/')
+print '\n'
 soup=BeautifulSoup(web)
 print soup.p.string
-print 'Email Valid For 10 minutes'
+print 'Email Valid For 10 minutes\n'
+
+t = threading.Timer(5.0, alarm)
+t.start() 
+
+def alarm():
+    print 'One Minute is Left\n'
+    print 'Need More Time, Extend by 10 Minutes'
+    print 'Enter Y for yes or N for Exit'
+    ExVar=raw_input()
+    proceed='y' or 'Y'
+    Exit='n' or 'N'
+    if ExVar==proceed:
+    #    urllib.urlopen('http://www.my10minutemail.com/Extend')
+     #   print soup.h1.span
+    else: exit()
+
 raw_input()
-#def alarm():
-#    print 'One Minute is Left'
-#t = threading.Timer(60.0, alarm)
-#t.start() 
+
+
+    
+
+
+
 
